6f4e84340140360c0ec9fa0633a25250bde81148
Lp700750 <2357543706@qq.com>
2022-12-28 20:37:01 +0800

    线程测试

--- 线程/pthread.c
+++ 线程/pthread.c
@@ -3,20 +3,26 @@
 #include<pthread.h>
 void* pthreadFunc(void* args)
 {
-	const char* p=(const char*)args;
-	while(1)
+	int p=*(int*)args;
+	printf("%d pthread pid=%d id=%lu\n",p,getpid(),pthread_self());
+	if(3==p)
 	{
-		printf("childPthread pid=%d\n",getpid());
-		sleep(1);
+		int* ptr=NULL;
+		*ptr=10;
 	}
+	sleep(1);
 }
 int main()
 {
-	pthread_t p;
-	pthread_create(&p,NULL,pthreadFunc,(void*)"childPthread");
+	pthread_t p[5];
 	while(1)
 	{
-		printf("parentPhtread pid=%d\n",getpid());
+		for(int i=0;i<5;++i)
+		{
+		    pthread_create(p+i,NULL,pthreadFunc,(void*)&i);
+		}
+
+		printf("parentPhtread pid=%d id=%lu\n",getpid(),pthread_self());
 		sleep(1);
 	}
 
